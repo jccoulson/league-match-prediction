e25de160a277d8a33caa2e1dabed3456c9cab643
jccoulson <jesseccoulson@gmail.com>
2024-03-18 02:53:25 -0700

    updating R eda and adding pdf of render

---
+++ league_statistics_eda.R
@@ -0,0 +1,12 @@
+---
+  title: "Enhanced League of Legends EDA"
+output: pdf_document
+---
+  
+  ```{r setup, include=FALSE}
+knitr::opts_chunk$set(echo = TRUE)
+library(tidyverse)
+library(lubridate)
+library(scales)
+library(ggplot2)
+library(forcats)
